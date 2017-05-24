c1c825d6ead1a0d4b91df5108d103ae1beebdccf
Kurisaki <kurisaki@temona.co.jp>
2017-05-24 16:20:57 +0900

    create user mypage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
   def show
+  	binding.pry
+  	@username = current_user.username
+  	@tweets = Tweet.where(user_id: current_user.id).page(params[:page]).per(5).order("created_at desc")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
 
-  get 'users/show'
 
   devise_for :users
   root 'tweets#index'
   get 'tweets' => 'tweets#index'
   get 'tweets/new' => 'tweets#new'
   post 'tweets' => 'tweets#create'
-  get 'users/:id' => 'users#show'
+  get 'users/:user_id' => 'users#show'
  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
